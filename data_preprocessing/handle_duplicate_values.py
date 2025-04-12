808b475a1a5f7bc3354b7722fecd8a0befb10c3b
samantim <saman.teymouri@gmail.com>
2025-04-12 17:30:09 +0200

    initial commit of handle duplicate values

---
+++ data_preprocessing/handle_duplicate_values.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import sys
+from os import path, makedirs
+import shutil
+import logging
+
+def config_logging():
+    # This function configs logging and prepares it for logging process
+    # Note that logging creates logs from every operation has been done in the program which is the more flexible, durable, and powerful than only using print
+    # Whenever you want a new logging, just delete the existing one!
+    logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(),
+        logging.FileHandler("all_operations.log")
+    ]
+    )
+
+
+def load_data(file_path : str) -> pd.DataFrame:
+    # Open csv file and load it into a dataframe
+    try:
+        data = pd.read_csv(file_path)
+    except:
+        logging.error("The path is invalid!")
+        return pd.DataFrame()
+
+    # Return the first 5 rows of the dataset
+    logging.info(data.head())
+
+    return data
+
+
+def handle_duplicate_values_drop(data : pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+def main():
+    # Start logging
+    config_logging()
+
+    # Check the argument passed to the program
+    # Note that the first argument is always the name of the program
+    if len(sys.argv) < 2:
+        logging.error("This program needs at least one parameter as the dataset path!")
+        sys.exit(0)
+    else:
+        match len(sys.argv):
+            case 2:
+                dataset_path = sys.argv[1]
+
+    # Load the dataset
+    original_data = load_data(dataset_path)
+    # If the dataset is not valid
+    if original_data.empty:
+        return
+    
+    # Create a folder for cleaned datasets
+    dataset_dir = path.dirname(dataset_path)
+    cleaned_data_dir = path.join(dataset_dir, "cleaned_data")
+    # Remove the directory if exists because some of the files may not need to create based on the program arguments
+    shutil.rmtree(cleaned_data_dir)
+    # Create the folder
+    makedirs(cleaned_data_dir, exist_ok=True)
+
+
+if __name__ == "__main__":
+    main()
